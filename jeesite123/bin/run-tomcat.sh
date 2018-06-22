2960252e58f25b0970284895e28092e027bd9f37
Xugai <xuqiubing@yeah.net>
2018-06-22 17:05:20 +0800

    jeesite123 was deleted

--- jeesite123/bin/run-tomcat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd ..
-
-MAVEN_OPTS=$MAVEN_OPTS -Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
-
-exec mvn clean spring-boot:run -U
--- jeesite123/src/main/java/com/jeesite/modules/sys/common/Const.java
+++
@@ -1,8 +0,0 @@
-package com.jeesite.modules.sys.common;
-
-/**
- * Created by rabbit on 2018/5/13.
- */
-public class Const {
-    public static final int DAY_MSEC = 86400000;    //一天24小时的毫秒数
-}
